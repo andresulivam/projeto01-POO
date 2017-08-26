b68fc0c2c503183dc49bb5cd37f5f2b6c0f89f10
André Sulivam <asulivam@gmail.com>
2017-08-26 16:06:34 -0300

    Remoção de hashCode

--- src/br/com/cotemig/entidades/ItemNotaFiscal.java
+++ src/br/com/cotemig/entidades/ItemNotaFiscal.java
@@ -55,19 +55,6 @@ public class ItemNotaFiscal {
 	}
 
 	/* (non-Javadoc)
-	 * @see java.lang.Object#hashCode()
-	 */
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((produto == null) ? 0 : produto.hashCode());
-		result = prime * result + ((quantidade == null) ? 0 : quantidade.hashCode());
-		result = prime * result + ((valorUnitario == null) ? 0 : valorUnitario.hashCode());
-		return result;
-	}
-
-	/* (non-Javadoc)
 	 * @see java.lang.Object#equals(java.lang.Object)
 	 */
 	@Override
